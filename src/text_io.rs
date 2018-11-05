e2b17a6dbf34c104a3fff86b7db6e60872cf2e26
Wilke Schwiedop <wilke.schwiedop.2010w@informatik.h-brs.de>
2018-11-05 13:20:05 +0100

    const -> mut

--- src/text_io.rs
+++ src/text_io.rs
@@ -7,21 +7,21 @@ use relation::rf_Relation;
 use relax::tex::ToTex;
 
 #[no_mangle]
-pub extern fn rf_set_to_string(s_ptr: *mut rf_Set) -> *const c_char {
+pub extern fn rf_set_to_string(s_ptr: *mut rf_Set) -> *mut c_char {
 	let s = unsafe { s_ptr.as_ref() }.unwrap();
 	let c_string = CString::new(s.to_string()).unwrap();
 	return CString::into_raw(c_string);
 }
 
 #[no_mangle]
-pub extern fn rf_relation_to_string(r_ptr: *mut rf_Relation) -> *const c_char {
+pub extern fn rf_relation_to_string(r_ptr: *mut rf_Relation) -> *mut c_char {
 	let r = unsafe { r_ptr.as_ref() }.unwrap();
 	let c_string = CString::new(r.to_string()).unwrap();
 	return CString::into_raw(c_string);
 }
 
 #[no_mangle]
-pub extern fn rf_relation_format_tex(r_ptr: *mut rf_Relation) -> *const c_char {
+pub extern fn rf_relation_format_tex(r_ptr: *mut rf_Relation) -> *mut c_char {
 	let r = unsafe { r_ptr.as_ref() }.unwrap();
 	let c_string = CString::new(r.to_tex().to_string()).unwrap();
 	return CString::into_raw(c_string);
